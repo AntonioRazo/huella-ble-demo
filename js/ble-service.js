581f31a4bfd3618b0ce979ed17d556f2cc338d61
AntonioRazo <50970239+AntonioRazo@users.noreply.github.com>
2025-09-30 01:31:35 -0600

    Refactor connect method to simplify error handling

--- js/ble-service.js
+++ js/ble-service.js
@@ -38,18 +38,12 @@ const BLEService = {
     
     // Connect to device
     async connect(device) {
-        try {
-            this.device = device;
-            this.server = await device.gatt.connect();
-            
-            console.log('Conectado al servidor GATT, esperando estabilización...');
-            await new Promise(resolve => setTimeout(resolve, 500)); // AGREGAR ESTE DELAY
-            
-            this.service = await this.server.getPrimaryService(this.SERVICE_UUID);
-            console.log('Servicio obtenido, obteniendo características...');
-            
-            // Get all characteristics con reintentos
-            try {
+         try {
+                this.device = device;
+                this.server = await device.gatt.connect();
+                this.service = await this.server.getPrimaryService(this.SERVICE_UUID);
+                
+                // Get all characteristics
                 this.characteristics.cmd = await this.service.getCharacteristic(this.CHAR_CMD_UUID);
                 this.characteristics.status = await this.service.getCharacteristic(this.CHAR_STATUS_UUID);
                 this.characteristics.data = await this.service.getCharacteristic(this.CHAR_DATA_UUID);
@@ -57,10 +51,7 @@ const BLEService = {
                 this.characteristics.info = await this.service.getCharacteristic(this.CHAR_INFO_UUID);
                 this.characteristics.params = await this.service.getCharacteristic(this.CHAR_PARAMS_UUID);
                 this.characteristics.sync = await this.service.getCharacteristic(this.CHAR_SYNC_UUID);
-            } catch (charError) {
-                console.error('Error obteniendo características:', charError);
-                throw charError;
-            }
+
             // Setup notifications
             await this.characteristics.status.startNotifications();
             this.characteristics.status.addEventListener('characteristicvaluechanged', 
